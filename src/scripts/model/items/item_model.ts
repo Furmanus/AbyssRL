f99f73c61a9f39ae9e4d823b6a3a31f0d61eb93a
Lukasz Lach <mietek76@gmail.com>
2019-10-02 00:05:06 +0200

    Add globalItemCollection and use it in ItemModel constructor

--- src/scripts/model/items/item_model.ts
+++ src/scripts/model/items/item_model.ts
@@ -1,6 +1,8 @@
 import {BaseModel} from '../../core/base_model';
 import {EntityModel} from '../entity/entity_model';
 import {ItemTypes} from '../../constants/item';
+import {IAnyObject} from '../../interfaces/common';
+import {globalItemCollection} from '../../global/collections';
 
 export abstract class ItemModel extends BaseModel {
     /**
@@ -20,6 +22,11 @@ export abstract class ItemModel extends BaseModel {
      */
     public abstract get fullDescription(): string;
 
+    public constructor(config: IAnyObject) {
+        super(config);
+
+        globalItemCollection.add(this);
+    }
     public drop(entity: EntityModel): void {
         // placeholder
     }
