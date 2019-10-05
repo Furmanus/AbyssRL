7f8589e005f795cded826c574475b40292e4ff9b
Lukasz Lach <llach@getresponse.com>
2019-10-05 21:52:38 +0200

    Add more weapon types

--- src/scripts/model/items/weapons/data/weapons.ts
+++ src/scripts/model/items/weapons/data/weapons.ts
@@ -2,30 +2,48 @@ export interface IWeaponConfigObject {
   damage: string;
   toHit: string;
   name: string;
-  type: string;
+  type: 'slashing' | 'piercing' | 'bludgeoning';
 }
 export interface IWeaponData {
   [type: string]: IWeaponConfigObject;
 }
 
 export const weaponsData: IWeaponData = {
+  short_sword: {
+    damage: `1d6`,
+    toHit: `1d4`,
+    name: `short sword`,
+    type: 'slashing',
+  },
   long_sword: {
     damage: `1d8`,
     toHit: `1d3`,
-    name: `long sword`,
+    name: `long_sword`,
     type: `slashing`,
   },
-  broad_axe: {
+  silver_sword: {
+    damage: `1d7-1`,
+    toHit: `1d3`,
+    name: `silver sword`,
+    type: 'slashing',
+  },
+  axe: {
     damage: `1d6+3`,
     toHit: `1d2`,
-    name: `broad axe`,
+    name: `axe`,
     type: `slashing`,
   },
-  short_spear: {
+  spear: {
     damage: `1d6`,
     toHit: `1d3`,
-    name: `short spear`,
+    name: `spear`,
     type: `piercing`,
+  },
+  mace: {
+    damage: `1d6+1`,
+    toHit: `1d3`,
+    name: `mace`,
+    type: 'bludgeoning',
   },
   morning_star: {
     damage: `1d7+2`,
@@ -40,9 +58,33 @@ export const weaponsData: IWeaponData = {
     type: `piercing`,
   },
   halberd: {
-    damage: `2d6`,
-    toHit: `1d1`,
+    damage: `2d7+1`,
+    toHit: `-1d2`,
     name: `halberd`,
     type: `slashing`,
+  },
+  club: {
+    damage: `1d6`,
+    toHit: `1d2`,
+    name: `club`,
+    type: `bludgeoning`,
+  },
+  two_handed_hammer: {
+    damage: `2d6+2`,
+    toHit: `-1d2`,
+    name: `two handed hammer`,
+    type: 'bludgeoning',
+  },
+  two_handed_sword: {
+    damage: `2d6+1`,
+    toHit: `1d1`,
+    name: `two handed sword`,
+    type: 'slashing',
+  },
+  two_handed_axe: {
+    damage: `2d6+4`,
+    toHit: `-1d1`,
+    name: `two handed axe`,
+    type: 'slashing',
   },
 };
