7fae7769d0a5cfc2503a27c2831b882624af2576
Lukasz Lach <lukasz.lach@getresponse.com>
2022-02-03 16:01:46 +0100

    refactor: remove obsolete tests

--- src/scripts/core/base_model.ts
+++ src/scripts/core/base_model.ts
@@ -2,8 +2,6 @@ import { Constructor } from './constructor';
 import { IAnyObject } from '../interfaces/common';
 
 export class BaseModel extends Constructor {
-  public id = Math.random().toString();
-
   public serialize(): unknown {
     return { ...this };
   }
